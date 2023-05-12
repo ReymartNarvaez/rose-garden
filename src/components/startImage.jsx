ed38a3cce114f8f25412c77dc96ad45d2ca85e19
ReymartNarvaez <reymart_narvaez@hotmail.com>
2023-05-12 12:18:28 +0200

    Add startImage component

---
+++ src/components/startImage.jsx
@@ -0,0 +1,10 @@
+export const StartImage = () => {
+  return (
+    <div className="absolute bg-[url('src/assets/image/Pental.png')] w-full bg-cover bg-no-repeat h-[250px] flex items-center justify-center px-10 text-lg font-extrabold text-[#FFFFFF] text-[53px]">
+      <ul className="absolute flex-col md:min-h-fit min-h-[60vh] w-1/2 flex items-center justify-center ">
+        <li className="m-5">Petals & Posies,</li>
+        <li className="m-2">A Floral Wonderland Awaits You</li>
+      </ul>
+    </div>
+  );
+};
